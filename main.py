7c9464ad7ed3f8578e08b32419ada4354068dbf8
Nabil Adnan <nabiladnan99@gmail.com>
2024-12-07 17:34:32 -0500

    checking if adding title metadata does anything in the docs

--- main.py
+++ main.py
@@ -72,7 +72,13 @@ async def update_item(item_id: str, item: Item):
 @app.get("/items")
 async def read_items(
     q: Annotated[
-        str | None, Query(min_length=3, max_length=50, pattern="^startswithsendswithh$")
+        str | None,
+        Query(
+            title="Some title",
+            min_length=3,
+            max_length=50,
+            pattern="^startswithsendswithh$",
+        ),
     ] = None,
 ):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
